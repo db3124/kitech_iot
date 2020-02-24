553465a6f14ab0abcf938a68bb89cf521a3f4edd
Jiyeun <57421994+db3124@users.noreply.github.com>
2020-02-24 15:02:14 +0900

    dht

--- Rasberry/200224/dht11_1_200224.py
+++ Rasberry/200224/dht11_1_200224.py
@@ -9,12 +9,15 @@ pin = 4
 try:
     while True:
         h, t = Adafruit_DHT.read_retry(sensor, pin)
+
         print(h)
         print(t)
+
         if h != None and t != None:
             print("{0:0.1f}\'C, {1:0.1f}%".format(t, h))
         else:
             print('Read error')
+            
         time.sleep(3)
 
 except KeyboardInterrupt:
